a5818586e014809e54460184c6522c4d26e5e0cf
AndSmi <developer@andsmi.ru>
2022-01-10 15:40:26 +0300

    Revert "Boilerplate for SSR"
    This reverts commit 06ad0a27eb4e5d33a8ac90ecd17100727a2a17f5.

--- app/App.jsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import { Page } from './components/Page';
-
-window.addEventListener('load', () => {
-    ReactDOM.hydrate(<Page />, document.getElementById('content'));
-});
